b19bb80e983ef896b4a04e305ac2036c11f8abbc
SarahMaiAuto <maiphuongdhbk1010@gmail.com>
2022-05-30 09:56:55 +0700

    Section 23

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,18 +1,42 @@
 import React from "react";
 
 class ChildComponent extends React.Component {
+  state = {
+    showJobs: false,
+  };
+
+  handleShowHide = () => {
+    this.setState({
+      showJobs: !this.state.showJobs,
+    });
+  };
+
   render() {
     let { name, arrJobs } = this.props;
+    let { showJobs } = this.state;
     return (
       <>
         I am child {name}
-        {arrJobs.map((item, index) => {
-          return (
-            <div key={item.id}>
-              {item.title} - {item.salary}
+        {showJobs === false ? (
+          <div>
+            <button onClick={(event) => this.handleShowHide()}>Show</button>
+          </div>
+        ) : (
+          <>
+            <div className="job-lists">
+              {arrJobs.map((item, index) => {
+                return (
+                  <div key={item.id}>
+                    {item.title} - {item.salary}
+                  </div>
+                );
+              })}
             </div>
-          );
-        })}
+            <div>
+              <button onClick={(event) => this.handleShowHide()}>Hide</button>
+            </div>
+          </>
+        )}
       </>
     );
   }
