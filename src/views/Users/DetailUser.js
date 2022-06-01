07441ea313d01bd79b3c8916690c8449b4472be2
Ngô Viết Thảo <torres2611@MacBook-Air-cua-Ngo.local>
2022-06-01 22:32:35 +0700

    Section 34

--- src/views/Users/DetailUser.js
+++ src/views/Users/DetailUser.js
@@ -1,23 +1,50 @@
 import React from "react";
-import { withRouter } from "react-router";
+import { withRouter } from "react-router-dom";
 import axios from "axios";
 
 class DetailUser extends React.Component {
-  state = {
-    user: {},
-  };
-  async componentDidMount() {
-    if (this.props.match && this.props.match.params) {
-      let id = this.props.match.params.id;
-      let res = axios.get(`https://reqres.in/api/users/${id}`);
-      this.setState({
-        user: res && res.data && res.data.data ? res.data.data : {},
-      });
+    state = {
+        user: {},
+    };
+    async componentDidMount() {
+        if (this.props.match && this.props.match.params) {
+            let id = this.props.match.params.id;
+            console.log("id: ", id);
+            let res = await axios.get(`https://reqres.in/api/users/${id}`);
+            this.setState({
+                user: res && res.data && res.data.data ? res.data.data : {},
+            });
+        }
     }
-  }
-  render() {
-    return <div> </div>;
-  }
+
+    handleClickBackBtn = () => {
+        this.props.history.push('/user');
+    }
+
+    render() {
+        let { user } = this.state;
+        let isEmptyObj = Object.keys(user).length === 0;
+        return (
+            <>
+                <div> Hello world from Detail User component with id: {this.props.match.params.id}</div>
+                {isEmptyObj === false ?
+                    <>
+                        <div>User's name: {user.first_name} {user.last_name}</div>
+                        <div>User's email: {user.email}</div>
+                        <div><img src={user.avatar} alt={user.avatar} /></div>
+                        <button type="button" onClick={() => this.handleClickBackBtn()}>Back</button>
+                    </>
+                    :
+                    <div>
+                        Do not have any user with id: {this.props.match.params.id}
+                    </div>
+
+                }
+
+            </>
+
+        )
+    }
 }
 
 export default withRouter(DetailUser);
--- src/views/Users/ListUser.js
+++ src/views/Users/ListUser.js
@@ -1,41 +1,46 @@
 import React from "react";
 import axios from "axios";
 import "./ListUser.scss";
+import { withRouter } from 'react-router-dom'
 
 class ListUser extends React.Component {
-  state = {
-    listUser: [],
-  };
-  async componentDidMount() {
-    let res = await axios.get("https://reqres.in/api/users?page=2");
-    this.setState({
-      listUser: res && res.data && res.data.data ? res.data.data : [],
-    });
-  }
+    state = {
+        listUser: [],
+    };
+    async componentDidMount() {
+        let res = await axios.get("https://reqres.in/api/users?page=2");
+        this.setState({
+            listUser: res && res.data && res.data.data ? res.data.data : [],
+        });
+    }
 
-  render() {
-    let { listUser } = this.state;
-    return (
-      <div className="list-user-container">
-        <div className="title">Fetch All List Users</div>
-        <div className="list-user-content">
-          {listUser && listUser.length > 0 ? (
-            listUser.map((user, index) => {
-              return (
-                <div className="child" key={user.id}>
-                  <span>
-                    {index + 1} - {user.first_name} - {user.last_name}
-                  </span>
+    hanldeOnclickOnUser = (user) => {
+        this.props.history.push(`/user/${user.id}`);
+    }
+
+    render() {
+        let { listUser } = this.state;
+        return (
+            <div className="list-user-container">
+                <div className="title">Fetch All List Users</div>
+                <div className="list-user-content">
+                    {listUser && listUser.length > 0 ? (
+                        listUser.map((user, index) => {
+                            return (
+                                <div className="child" key={user.id} onClick={() => this.hanldeOnclickOnUser(user)}>
+                                    <span>
+                                        {index + 1} - {user.first_name} - {user.last_name}
+                                    </span>
+                                </div>
+                            );
+                        })
+                    ) : (
+                        <div>Do not have any user</div>
+                    )}
                 </div>
-              );
-            })
-          ) : (
-            <div>Do not have any user</div>
-          )}
-        </div>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
-export default ListUser;
+export default withRouter(ListUser);
